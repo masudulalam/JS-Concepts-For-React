b1b7b353ddd9de3bcccb9740487933c0542fe639
masudulalam <masuudulalam@gmail.com>
2024-12-25 23:27:33 +0600

    added fundamental.js

---
+++ fundamentals.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 
+ * - Variable
+    - How to declare a variable using let and const keyword.
+- 2. Condition
+    - 6 Basic Condition: >, <, ===, ! ==, ≥, ≤.
+    - Multiple Condition: &&, ||.
+- 3. Array
+    - How to Declare Array?
+    - What is an Array index?
+    - Set an array element by index.
+    - Array Length.
+    - Array Push Method.
+- 4. Loop
+    - for loop.
+    - while loop.
+    
+- 5. Function
+    - How to Declare a Normal Function?
+    - How to Call a Function?
+    - How to Return a Value from Function?
+- Object
+    - How to Declare an Object?
+    - 3 Ways to Access Property by Name:
+        - Direct by  Property name using dot method: object.propertyName.
+        - Via Property Name String: object[’propertyName’].
+        - Via Property Name in a Variable: object[variableName].
+ */
