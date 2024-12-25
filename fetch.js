fe5ee01f1a1ca4503e4eeb8149deda47e4d2ab26
masudulalam <masuudulalam@gmail.com>
2024-12-26 03:29:08 +0600

    explore how to copy an array and add element in copied array using destructuring

--- fetch.js
+++ fetch.js
@@ -19,8 +19,8 @@ const studentJSON = JSON.stringify(student);
 // console.log(student);
 // console.log(studentJSON);
 
-const studentObject = JSON.parse(studentJSON);
-console.log(studentObject);
+// const studentObject = JSON.parse(studentJSON);
+// console.log(studentObject);
 
 
 
@@ -32,5 +32,29 @@ console.log(studentObject);
 // keys, values
 const keys = Object.keys(student);
 const values = Object.values(student);
-console.log(keys);
-console.log(values);
+// console.log(keys);
+// console.log(values);
+
+
+// array.forEach()
+const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(num => console.log(num));
+
+// array.map()
+// numbers.map(num => console.log(num * 2));
+
+
+// add or remove from an array
+const dailyProducts = [
+    {name: 'Rice', price: 50, color: 'brown', production: 'natural', brand:'Muslim Crops'},
+    {name: 'Wheat', price: 40, color: 'brown', production: 'natural', brand:'Muslim Crops'},
+    {name: 'Musuri Dal', price: 90, color: 'red', production: 'natural', brand:'Muslim Crops'},
+    {name: 'Milk', price: 50, color:'', production: 'natural', brand:'Muslim Protein'},
+    {name: 'Egg', price: 10, color: 'white', production: 'natural', brand:'Muslim Protein'},
+    {name: 'Musturd oil', price: 150, color: 'white', production: 'natural', brand:'Muslim Oil'},
+]
+
+const newProduct = {name: 'Broccoli', price: 40, color: 'green', production: 'natural', brand:'Muslim Vegetables'};
+// copy dailyProducts array and then add new product
+const newProducts = [...dailyProducts, newProduct];
+console.log(newProducts);
