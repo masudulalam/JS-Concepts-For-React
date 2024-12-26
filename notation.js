472017ff7a0ce18916fe5bc5c7ef30ce33991f7c
masudulalam <masuudulalam@gmail.com>
2024-12-26 20:41:24 +0600

    recap dot notation and bracket notation for accessing object properties

---
+++ notation.js
@@ -0,0 +1,14 @@
+person = {
+    name: 'Abdullah',
+    age: 6,
+    profession: 'Student',
+    live: 'Lohagara'
+}
+
+// Dot Notation
+const prof1 = person.profession;
+console.log(prof1);
+
+// Bracket Notation
+const prof2 = person['profession'];
+console.log(prof2);
